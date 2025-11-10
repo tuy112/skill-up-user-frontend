3b5f62ae216719dcd1ae3aa9b3e4db7477bdbf4f
Kim Jae Hyeok <master_catch@naver.com>
2025-11-10 20:44:28 +0900

    fix : layout.tsx 정리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,24 @@
+import type { Metadata } from "next";
 import "@/styles/global.css";
-import { ReactNode } from "react";
 
-
-export const metadata = {
-  title: "skill Up User Front",
+export const metadata: Metadata = {
+  title: {
+    default: "Skill Up User",
+    template: "%s | Skill Up User",
+  },
   description: "스킬업 웹 구축 프로젝트",
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko">
       <head>
         <link
           rel="stylesheet"
-          as="style"
-          crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/variable/pretendardvariable.min.css"
         />
       </head>
